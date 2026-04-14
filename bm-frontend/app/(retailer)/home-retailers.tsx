2ce5f008e7c78135dc19cd86783b818bcefea485
Jennyl51 <jennyl5118@berkeley.edu>
2026-04-13 19:46:31 -0700

    add button to home

--- bm-frontend/app/(retailer)/home-retailers.tsx
+++ bm-frontend/app/(retailer)/home-retailers.tsx
@@ -1,9 +1,43 @@
-import { View, Text } from 'react-native';
+import { View, Pressable, StyleSheet, Text } from 'react-native';
+import { Link } from 'expo-router';
 
 export default function RetailerDashboard() {
   return (
     <View>
       <Text>Home</Text>
+
+    <Link href="/retailer-dashboard" asChild>
+    <Pressable style={styles.button}>
+    <Text style={styles.buttonText}>Retailer Dashboard</Text>
+    </Pressable>
+  </Link>
     </View>
+
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    backgroundColor: '#f9fafb',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '700',
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: '#2563eb',
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    width: 200,
+    alignItems: 'center',
+    marginBottom: 30,
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: '600',
+  },
+});
